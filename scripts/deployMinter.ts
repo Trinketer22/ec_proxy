659be4091241a350b693a4d85940ff300d97fb46
Trinketer22 <trinketer22@localhost>
2024-12-20 19:38:34 +0300

    Simple deploy script

--- scripts/deployMinter.ts
+++ scripts/deployMinter.ts
@@ -1,11 +1,32 @@
-import { toNano } from '@ton/core';
-import { Minter } from '../wrappers/Minter';
+import { toNano, Address } from '@ton/core';
+import { jettonContentToCell, Minter } from '../wrappers/Minter';
 import { compile, NetworkProvider } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const minter = provider.open(Minter.createFromConfig({}, await compile('Minter')));
+    const walletCode = await compile('Wallet');
+    if(!process.env.ADMIN_ADDRESS) {
+        throw Error("ADMIN_ADDRESS env is required");
+    }
+    if(!process.env.CURRENCY_ID) {
+        throw Error("CURRENCY_ID env is required");
+    }
 
-    await minter.sendDeploy(provider.sender(), toNano('0.05'));
+    const content = jettonContentToCell({
+        type: 'onchain',
+        data: {
+            uri:'http://localhost/meta.json',
+            decimals: '9',
+            description: 'My test jetton'
+        }
+    });
+    const minter = provider.open(Minter.createFromConfig({
+        admin: Address.parse(process.env.ADMIN_ADDRESS),
+        curId: BigInt(process.env.CURRENCY_ID),
+        content: content,
+        walletCode
+    }, await compile('Minter')));
+
+    await minter.sendDeploy(provider.sender(), toNano('0.15'));
 
     await provider.waitForDeploy(minter.address);
 
