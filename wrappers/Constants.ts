ff2faaaebc583e75a07cbbfce0348a860d14f093
Trinketer22 <trinketer22@localhost>
2025-01-18 23:04:53 +0300

    Update message schemes

--- wrappers/Constants.ts
+++ wrappers/Constants.ts
@@ -1,6 +1,6 @@
 export const Ops = {
     minter: {
-       deploy_wallet: 0x44bb3e46,
+       deploy_wallet: 0xad9d230a,
        provide_wallet_address: 0x2c76b973,
        take_wallet_address: 0xd1735400,
        drop_admin: 0x67a18fb6,
@@ -8,13 +8,13 @@ export const Ops = {
     },
 
     wallet: {
-       internal_deploy : 0x6a4de08d,
+       internal_deploy : 0xa97ca079,
        excesses : 0xd53276db,
        transfer_notification : 0x7362d09c,
        ton_refund : 0xe41f7d83,
        ec_transfer : 0x68039ead,
        transfer : 0x0f8a7ea5,
-       update_forward_gas : 0xf6f24f33,
+       update_proxy_options: 0xe87b5bd5,
        withdraw_extra : 0x7ad2441e
     }
 }
